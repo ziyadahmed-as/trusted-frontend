7a1741466df27ba225c013e23d17e33254c12aed
Ziyad Ahmed <ziyadwru.12@gmail.com>
2026-04-10 17:00:24 +0300

    updateproduct page

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,5 +1,7 @@
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
+// In dev, we use the absolute URL to avoid potential issues with Next.js rewrites and trailing slashes.
+console.log('API_URL:', API_URL);
 
 export const apiClient = {
   async login(credentials: any) {
--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8000/api/:path*',
+        destination: 'http://127.0.0.1:8000/api/:path*',
       },
     ];
   },
